6c2c740c56358f5898ef99019ec854789912527f
archillexx <ashwinsuresh.x@gmail.com>
2021-10-17 17:04:29 +0530

    Inital Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+var items = []
+var workItems = []
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+	var today = new Date()
+
+	var options = {
+		weekday: 'long',
+		day: 'numeric',
+		month: 'long',
+	}
+
+	var day = today.toLocaleDateString('en-US', options)
+
+	res.render('list', {
+		listTitle: day,
+		newListItems: items,
+	})
+})
+
+app.post('/', function (req, res) {
+	var item = req.body.newItem
+	if (req.body.list === 'Work') {
+		workItems.push(item)
+		res.redirect('/work')
+	} else {
+		items.push(item)
+		res.redirect('/')
+	}
+})
+
+app.get('/work', function (req, res) {
+	res.render('list', { listTitle: 'Work List', newListItems: workItems })
+})
+
+app.listen(3000, function () {
+	console.log('Server runing successfully!!!')
+})
